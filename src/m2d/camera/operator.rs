16188a335e34f20f1078a8a1d1c695612db9dfe2
Nazariglez <nazari.nz@gmail.com>
2023-02-25 23:39:10 +0000

    minor changes

--- src/m2d/camera/operator.rs
+++ src/m2d/camera/operator.rs
@@ -1,16 +1,19 @@
 use notan::math::{vec2, Rect, Vec2};
 
+use crate::tween;
+
 use super::Camera;
 
 pub struct CameraOperator {
     pub camera: Camera,
-    pub director: CameraDirector,
+    pub style: CameraStyle,
     target_pos: Vec2,
+    target_zoom: f32,
 }
 
 impl CameraOperator {
     pub fn new(camera: Camera) -> Self {
-        let director = CameraDirector {
+        let style = CameraStyle {
             limit: Some(Rect {
                 x: 0.0,
                 y: 0.0,
@@ -22,8 +25,9 @@ impl CameraOperator {
 
         Self {
             camera,
-            director,
+            style,
             target_pos: Vec2::ZERO,
+            target_zoom: 1.0,
         }
     }
 
@@ -31,27 +35,45 @@ impl CameraOperator {
         self.target_pos = vec2(x, y);
     }
 
-    pub fn zoom_in(&mut self, factor: f32) {}
-    pub fn zoom_out(&mut self, factor: f32) {}
+    pub fn zoom_in(&mut self, factor: f32) {
+        self.target_zoom -= factor;
+    }
+
+    pub fn zoom_out(&mut self, factor: f32) {
+        self.target_zoom += factor;
+    }
+
+    pub fn set_zoom(&mut self, zoom: f32) {
+        self.target_zoom = zoom;
+    }
+
     pub fn shake(&mut self, pixels: f32, time: f32) {}
 
     pub fn update(&mut self, delta: f32) {
-        let mut camera_pos = self.camera.position();
+        let camera_pos = self.camera.position();
         if camera_pos == self.target_pos {
             return;
         }
 
-        println!("11111");
-        dbg!(self.target_pos, camera_pos);
+        let x = if self.style.axis_x.lock {
+            camera_pos.x
+        } else {
+            self.target_pos.x
+        };
+
+        let y = if self.style.axis_y.lock {
+            camera_pos.y
+        } else {
+            self.target_pos.y
+        };
 
         self.camera
             .set_position(self.target_pos.x, self.target_pos.y);
-        // self.camera.update();
 
         // check limits
-        if let Some(limit) = self.director.limit {
+        if let Some(limit) = self.style.limit {
             let camera_bounds = self.camera.bounds();
-            let min_x_diff = dbg!(limit.min_x() - camera_bounds.min_x());
+            let min_x_diff = limit.min_x() - camera_bounds.min_x();
             let max_x_diff = limit.max_x() - camera_bounds.max_x();
             let min_y_diff = limit.min_y() - camera_bounds.min_y();
             let max_y_diff = limit.max_y() - camera_bounds.max_y();
@@ -70,26 +92,38 @@ impl CameraOperator {
             }
 
             if pos_offset != Vec2::ZERO {
-                dbg!(camera_bounds);
-                dbg!(pos_offset);
                 self.target_pos += pos_offset;
                 self.camera
                     .set_position(self.target_pos.x, self.target_pos.y);
             }
         }
 
-        println!("22222");
-        dbg!(self.target_pos, self.camera.position());
         self.camera.update();
     }
     // pub fn rotate
 }
 
+pub struct CameraAxis {
+    lock: bool,
+    lag: f32,
+    easing: tween::EaseFn,
+}
+
+impl Default for CameraAxis {
+    fn default() -> Self {
+        Self {
+            lock: false,
+            lag: 0.0,
+            easing: tween::LINEAR,
+        }
+    }
+}
+
 #[derive(Default)]
-pub struct CameraDirector {
+pub struct CameraStyle {
     limit: Option<Rect>,
-    lock_x: bool,
-    lock_y: bool,
+    axis_x: CameraAxis,
+    axis_y: CameraAxis,
 }
 
 // directives? toip down? lock horizontal? limits?
