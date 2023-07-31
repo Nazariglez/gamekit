a34ef513f04d96b0310a6377537efc5355fcb0c3
Nazari Gonzalez <nazari@platoteam.com>
2023-07-31 23:56:13 +0100

    wip

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -7,16 +7,31 @@ use gamekit::prelude::*;
 
 // language=wgsl
 const SHADER: &str = r#"
+// Vertex shader
+struct VertexInput {
+    @location(0) position: vec3<f32>,
+    @location(1) color: vec3<f32>,
+};
+
+struct VertexOutput {
+    @builtin(position) clip_position: vec4<f32>,
+    @location(0) color: vec3<f32>,
+};
+
 @vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+fn vs_main(
+    model: VertexInput,
+) -> VertexOutput {
+    var out: VertexOutput;
+    out.color = model.color;
+    out.clip_position = vec4<f32>(model.position, 1.0);
+    return out;
 }
 
+// Fragment shader
 @fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 1.0, 0.0, 1.0);
+fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+    return vec4<f32>(in.color, 1.0);
 }
 "#;
 
@@ -32,16 +47,16 @@ impl State {
             .create_render_pipeline(SHADER)
             .with_vertex_layout(
                 VertexLayout::new()
-                    .with_attr(0, VertexFormat::Float32x2)
+                    .with_attr(0, VertexFormat::Float32x3)
                     .with_attr(1, VertexFormat::Float32x3),
             )
             .build()?;
 
         #[rustfmt::skip]
         let vertices = [
-            0.0, 0.0,   0.0, 0.0, 0.0,
-            0.0, 0.0,   0.0, 0.0, 0.0,
-            0.0, 0.0,   0.0, 0.0, 0.0,
+            0.0, 0.5, 0.0,      1.0, 0.0, 0.0,
+            -0.5, -0.5, 0.0,    0.0, 1.0, 0.0,
+            0.5, -0.5, 0.0,     0.0, 0.0, 1.0,
         ];
 
         let vbo = gfx.create_vertex_buffer(&vertices).build()?;
@@ -60,7 +75,7 @@ fn main() -> Result<(), String> {
 
 fn on_draw(evt: &event::Draw, gfx: &mut Gfx, state: &mut State) {
     let mut renderer = Renderer::new();
-    renderer.begin(Color::RED, 0, 0);
+    renderer.begin(Color::WHITE, 0, 0);
     renderer.apply_pipeline(&state.pip);
     let bindings = [&state.vbo];
     renderer.apply_bindings(&bindings);
