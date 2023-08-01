da60b42ad9ba8eb3c155676ae19433a1108e1dff
Nazari Gonzalez <nazari@platoteam.com>
2023-08-01 12:07:34 +0100

    minor changes

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -7,14 +7,13 @@ use gamekit::prelude::*;
 
 // language=wgsl
 const SHADER: &str = r#"
-// Vertex shader
 struct VertexInput {
     @location(0) position: vec2<f32>,
     @location(1) color: vec3<f32>,
 };
 
 struct VertexOutput {
-    @builtin(position) clip_position: vec4<f32>,
+    @builtin(position) position: vec4<f32>,
     @location(0) color: vec3<f32>,
 };
 
@@ -24,11 +23,10 @@ fn vs_main(
 ) -> VertexOutput {
     var out: VertexOutput;
     out.color = model.color;
-    out.clip_position = vec4<f32>(model.position - 0.5, 1.0, 1.0);
+    out.position = vec4<f32>(model.position - 0.5, 1.0, 1.0);
     return out;
 }
 
-// Fragment shader
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
     return vec4<f32>(in.color, 1.0);
