24d4c6169b43104f181a756a25faa02d634ddcd9
Nazari Gonzalez <nazari@platoteam.com>
2023-08-02 23:22:35 +0100

    max_uniform_buffers

--- crates/gk_gfx/src/consts.rs
+++ crates/gk_gfx/src/consts.rs
@@ -2,3 +2,9 @@ pub const MAX_VERTEX_BUFFERS: usize = 8;
 pub const MAX_VERTEX_ATTRIBUTES: usize = 16;
 pub const MAX_SAMPLERS_PER_SHADER_STAGE: usize = 16;
 pub const MAX_SAMPLED_TEXTURES_PER_SHADER_STAGE: usize = 16;
+
+#[cfg(all(target_arch = "wasm32", feature = "webgl"))]
+pub const MAX_UNIFORM_BUFFERS_PER_SHADER_STAGE: usize = 11;
+
+#[cfg(not(any(target_arch = "wasm32", feature = "webgl")))]
+pub const MAX_UNIFORM_BUFFERS_PER_SHADER_STAGE: usize = 12;
