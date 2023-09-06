4fb8eca6db9b67176001089128dbc593bc868153
Nazarí González <nazari.nz@gmail.com>
2023-09-06 21:13:17 +0100

    minor change

--- examples/gfx_clear.rs
+++ examples/gfx_clear.rs
@@ -7,11 +7,11 @@ fn main() -> Result<(), String> {
         .add_config(Platform::config())?
         .add_config(Gfx::config())?
         .add_config(Time::config())?
-        .on(on_frame)
+        .on(on_draw)
         .build()
 }
 
-fn on_frame(frame: &DrawFrame, gfx: &mut Gfx, time: &mut Time) {
+fn on_draw(frame: &DrawFrame, gfx: &mut Gfx, time: &mut Time) {
     let color = Color::rgb(time.elapsed_f32().cos(), time.elapsed_f32().sin(), 1.0);
     let mut renderer = frame.create_renderer();
     renderer.clear(Some(color), None, None);
